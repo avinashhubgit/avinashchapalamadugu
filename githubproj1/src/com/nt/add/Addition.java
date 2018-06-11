dae658b6c88fce00948e39c343c4e1bec68a40f9
Admin <Admin@192.168.0.12>
2018-06-11 23:32:21 +0530

    tlmake2change

--- githubproj1/src/com/nt/add/Addition.java
+++ githubproj1/src/com/nt/add/Addition.java
@@ -2,7 +2,7 @@ package com.nt.add;
 
 public class Addition {
   static int number1=10;
-  static int number2=20;
+  static int number2=200;
 	public static void main(String[] args) {
 		
 System.out.println(number1+number2);

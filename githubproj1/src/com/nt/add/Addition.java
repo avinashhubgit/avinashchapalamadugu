4b15be0c112a507c906557a82e019eb4d98a00f9
Admin <Admin@192.168.0.12>
2018-06-11 22:38:18 +0530

    TL1-commit

---
+++ githubproj1/src/com/nt/add/Addition.java
@@ -0,0 +1,11 @@
+package com.nt.add;
+
+public class Addition {
+  static int number1=10;
+  static int number2=20;
+	public static void main(String[] args) {
+		
+System.out.println(number1+number2);
+	}
+
+}

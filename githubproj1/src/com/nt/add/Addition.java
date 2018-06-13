d2117f7646c8e453301f1d1128e0c4a53617224b
hp <hp@DESKTOP-2AR07GG>
2018-06-13 18:01:35 +0530

    edit by hemanth

--- githubproj1/src/com/nt/add/Addition.java
+++ githubproj1/src/com/nt/add/Addition.java
@@ -1,11 +1,8 @@
 package com.nt.add;
-
-public class Addition {
-  static int number1=10;
-  static int number2=20;
-	public static void main(String[] args) {
-		
-System.out.println(number1+number2);
-	}
-
-}
+ public class Addition
+ {
+	 public static void main(String[] args)
+	 {
+		 System.out.println("csdscjsjchshd");
+	 }
+ }

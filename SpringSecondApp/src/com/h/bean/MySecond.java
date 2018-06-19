0081a8e7dcf03cd6e0bcb2e7664ea739120489cf
hp <hp@DESKTOP-2AR07GG>
2018-06-19 15:41:39 +0530

    zsc

---
+++ SpringSecondApp/src/com/h/bean/MySecond.java
@@ -0,0 +1,22 @@
+package com.h.bean;
+
+import java.util.Scanner;
+
+public class MySecond {
+private Scanner scr;
+public void setScr(Scanner scr)
+{
+this.scr=scr;	
+}
+public void use()
+{
+int value=0;
+System.out.println("your amount is 100 enter your amount ;;");
+value=Integer.parseInt(scr.nextLine());
+System.out.println(value);
+if(value<=100)
+{
+	System.out.println("you prduct is delivered in 3 days by dtdc coreor");
+	}
+}
+}
---
+++ SpringSecondApp/src/com/h/spng/SecondDemo.java
@@ -0,0 +1,29 @@
+package com.h.spng;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.h.bean.MySecond;
+
+public class SecondDemo {
+public static void main(String[] args)
+{
+Resource res=null;
+BeanFactory factory=null;
+Object obj=null;
+MySecond ms=null;
+//load xml file
+System.out.println("11111111111");
+res=new FileSystemResource("src/com/h/cngf/appilicationContext.Xml");
+System.out.println("222222222222222222222");
+//create ioc container
+factory=new XmlBeanFactory(res);
+System.out.println("444444444");
+obj=factory.getBean("sec",MySecond.class);
+System.out.println("3333333333333");
+ms=(MySecond)obj;
+ms.use();
+}
+}
